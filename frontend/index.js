36271c645ee416f5d310332e280f9d12d442a588
qhhdy <kimby0406@naver.com>
2024-05-08 01:45:54 +0900

    ✨글쓰기 페이지 기능 구현
    -프론트엔드 글쓰기 페이지 form으로 작업
    -백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능을 구현

---
+++ frontend/index.js
@@ -0,0 +1,67 @@
+const calcTime = (timestamp) => {
+  // 한국시간 UTC + 9시간
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+  const time = new Date(curTime - timestamp);
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  if (hour > 0) return `${hour}시간 전`;
+  else if (minute > 0) return `${minute}분 전`;
+  else if (second > 0) return `${second}초 전`;
+  else "방금 전";
+};
+
+const renderData = (data) => {
+  const main = document.querySelector("main");
+  //['a','b','c'].reverse()
+  //=['c','b','a']
+  data
+    .sort((a, b) => a)
+    .reverse()
+    .forEach(async (obj) => {
+      const div = document.createElement("div");
+      div.className = "item-list";
+
+      const imgDiv = document.createElement("div");
+      imgDiv.className = "item-list__img";
+
+      //이미지 불러오기
+      const img = document.createElement("img");
+      const res = await fetch(`/images/${obj.id}`);
+      const blob = await res.blob();
+      const url = URL.createObjectURL(blob);
+      img.src = url;
+
+      const InfoDiv = document.createElement("div");
+      InfoDiv.className = "item-list__info";
+
+      const InfoTitleDiv = document.createElement("div");
+      InfoTitleDiv.className = "item-list__info-title";
+      InfoTitleDiv.innerText = obj.title;
+
+      const InfoMetaDiv = document.createElement("div");
+      InfoMetaDiv.className = "item-list__info-meta";
+      InfoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
+
+      const InfoPriceDiv = document.createElement("div");
+      InfoPriceDiv.className = "item-list__info-price";
+      InfoPriceDiv.innerText = obj.price;
+
+      imgDiv.appendChild(img);
+      InfoDiv.appendChild(InfoTitleDiv);
+      InfoDiv.appendChild(InfoMetaDiv);
+      InfoDiv.appendChild(InfoPriceDiv);
+      div.appendChild(imgDiv);
+      div.appendChild(InfoDiv);
+      main.appendChild(div);
+    });
+};
+
+const fetchList = async () => {
+  const res = await fetch("/items");
+  const data = await res.json();
+  renderData(data);
+};
+
+fetchList();
---
+++ frontend/write.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("write-form");
+
+const handleSubmitForm = async (event) => {
+  event.preventDefault();
+  const body = new FormData(form);
+  // 세계시간 UTC
+  body.append("insertAt", new Date().getTime());
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body: body,
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+form.addEventListener("submit", handleSubmitForm);
