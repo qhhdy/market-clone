a91633cb083574cae081be9651f67c7230e1fb07
qhhdy <kimby0406@naver.com>
2024-05-08 01:56:11 +0900

    ✨
    사진 업로드 수정

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ async def get_image(item_id):
     image_bytes = cur.execute(f"""
                             SELECT image from items WHERE id={item_id}
                             """).fetchone()[0]
-    return Response(content=bytes.fromhex(image_bytes))
+    
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
